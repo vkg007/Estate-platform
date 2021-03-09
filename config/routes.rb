570692615b73606cdaf6d01d7935f4ce601b2102
vkg007 <vkg0072013@gmail.com>
2021-03-09 13:28:57 +0530

    Applying Bootstrap to UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'main#index'
+  get 'logout', to: 'sessions#destroy'
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 end
+

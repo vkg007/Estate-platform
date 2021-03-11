9cf08713c873279b0c6f70894f9b82200c2d6f1f
vkg007 <vkg0072013@gmail.com>
2021-03-11 19:07:06 +0530

    Adding User edit Functionality and Enhancing UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [ :edit, :update]
+
   def new
     @user = User.new
   end
+
+  def edit; end
 
   def create
     @user = User.new(user_params) 
@@ -14,9 +18,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'Edited successfully.' 
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :mobile, :password, :password_confirmation, :pic)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'main#index'
   get 'logout', to: 'sessions#destroy'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
   resources :sessions, only: [:new, :create]
 end
 

fa3b50ae9fc7997ddf91620d03b8e967203b501a
vkg007 <vkg0072013@gmail.com>
2021-03-12 21:24:24 +0530

    Applying Old password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [ :edit, :update]
+  before_action :find_user, only: %i[edit update]
 
   def new
     @user = User.new
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
   def edit; end
 
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] =  'You have successfully created an account!'
+      flash[:success] = 'You have successfully created an account!'
       redirect_to root_path
     else
       render :new
@@ -19,10 +19,15 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
-      flash[:success] = 'Edited successfully.' 
-      redirect_to root_path
+    if @user&.authenticate(user_update_params[:old_password])
+      if @user.update(user_params)
+        flash[:success] = 'Edited successfully.'
+        redirect_to root_path
+      else
+        render :edit
+      end
     else
+      flash[:error] = 'Invalid password'
       render :edit
     end
   end
@@ -31,6 +36,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :mobile, :password, :password_confirmation, :pic)
+  end
+
+  def user_update_params
+    params.require(:user).permit(:old_password)
   end
 
   def find_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'main#index'
   get 'logout', to: 'sessions#destroy'
-  resources :users, only: [:new, :create, :edit, :update]
-  resources :sessions, only: [:new, :create]
+  resources :users, only: %i[new create edit update]
+  resources :sessions, only: %i[new create]
 end
-

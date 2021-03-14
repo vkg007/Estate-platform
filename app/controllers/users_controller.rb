f5964458eb62eb0142821bddf6b71a7b4032a2fd
Vivek Gupta <55463168+vkg007@users.noreply.github.com>
2021-03-14 15:38:59 +0530

    Added the User details Edit option and Enhanced the Header (#4)
    * Adding User edit Functionality and Enhancing UI
    
    * Applying Old password confirmation
    
    * Applying exception case and space check
    
    * Applying privacy control to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,34 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[edit update]
+
   def new
     @user = User.new
   end
 
+  def edit; end
+
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] =  'You have successfully created an account!'
+      flash[:success] = 'You have successfully created an account!'
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def update
+    if @user&.authenticate(user_update_params[:old_password])
+      if @user.update(user_params)
+        flash[:success] = 'Edited successfully.'
+        redirect_to root_path
+      else
+        render :edit
+      end
+    else
+      flash[:error] = 'Invalid password'
+      render :edit
     end
   end
 
@@ -18,5 +36,17 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :mobile, :password, :password_confirmation, :pic)
+  end
+
+  def user_update_params
+    params.require(:user).permit(:old_password)
+  end
+
+  def find_user
+    @user = User.find_by_id(params[:id])
+    return if current_user == @user
+
+    flash[:error] = 'User not found/You can edit only your details'
+    redirect_to root_path
   end
 end

8458135a07c273882620dcfefc0c1fe55327b537
vkg007 <vkg0072013@gmail.com>
2021-03-18 13:34:40 +0530

    refactoring user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :properties, dependent: :delete_all
+  has_many :properties, dependent: :destroy
   validates :email, uniqueness: true, presence: true
   validates :mobile, uniqueness: true, presence: true
   validates :password, presence: true, confirmation: true, length: { within: 6..40 }, on: :create

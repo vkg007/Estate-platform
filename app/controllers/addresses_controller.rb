d3138ca7d7cc15cd913202ae2d518df0f28c89fe
vkg007 <vkg0072013@gmail.com>
2021-03-18 11:56:53 +0530

    User & property can have image now

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,20 +1,20 @@
 class AddressesController < ApplicationController
   before_action :find_addressable
-  before_action :find_address, only: %i[edit update]
+  before_action :find_address, only: %i[edit update destroy]
 
   def new
-    @address = @addressable_type.new if params[:user_id]
+    @address = @addressable.addresses.new if params[:user_id]
     @address = Address.new if params[:property_id]
   end
 
   def index
-    @addresses = @addressable_type
+    @addresses = @addressable.addresses
   end
 
   def edit; end
 
   def create
-    @address = @addressable_type.new(address_params) if params[:user_id]
+    @address = @addressable.addresses.new(address_params) if params[:user_id]
     @address = @addressable.build_address(address_params) if params[:property_id]
 
     if @address.save
@@ -37,7 +37,6 @@ class AddressesController < ApplicationController
   end
 
   def destroy
-    @address = Address.find(params[:id])
     if @address.destroy
       flash[:success] = 'Address details deleted successfully.'
       redirect_to root_path
@@ -54,21 +53,23 @@ class AddressesController < ApplicationController
   end
 
   def find_address
-    @address = @addressable_type.find_by_id(params[:id]) if params[:user_id]
-    @address = @addressable_type if params[:property_id]
+    @address = @addressable.addresses.find_by_id(params[:id]) if params[:user_id]
+    @address = @addressable.address if params[:property_id]
+    return if @address
+
+    flash[:error] = 'Address Not Found'
+    redirect_to root_path
   end
 
   def find_addressable
     if params[:user_id]
       @addressable = User.find_by_id(params[:user_id])
-      @addressable_type = @addressable.addresses
       return if current_user == @addressable
 
       flash[:error] = "You don't have rights"
       redirect_to root_path
     elsif params[:property_id]
       @addressable = Property.find_by_id(params[:property_id])
-      @addressable_type = @addressable.address
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :mobile, :password, :password_confirmation, :pic)
+    params.require(:user).permit(:first_name, :last_name, :email, :mobile, :password, :password_confirmation, :image)
   end
 
   def user_update_params

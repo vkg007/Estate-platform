84b0f09046aa2fa5d5d1a267644fa6e588e06650
Vivek Gupta <55463168+vkg007@users.noreply.github.com>
2021-03-19 17:26:20 +0530

    User Can Have Profile Pic and Property can have Multiple Images for Preview (#7)
    * Adding new model property with CRUD and associations
    
    * User can add many address and property can have  one address
    
    * Removed => symbols and error message
    
    * User & property can have image now
    
    * Removing head tag
    
    * Modifing loops

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-
+ 
+  has_one_attached :image, dependent: :destroy
   has_many :properties, dependent: :destroy
   has_many :addresses, as: :addressable, dependent: :destroy
+
   validates :email, uniqueness: true, presence: true
   validates :mobile, uniqueness: true, presence: true
   validates :password, presence: true, confirmation: true, length: { within: 6..40 }, on: :create

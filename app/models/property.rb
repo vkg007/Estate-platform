937409e67c572b5c538369b71d632cde218d39d7
vkg007 <vkg0072013@gmail.com>
2021-03-20 11:01:28 +0530

    Refactoring codes

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ApplicationRecord
   belongs_to :user
-  has_one :contract
+  has_one :contract, dependent: :destroy
   has_one :address, as: :addressable, dependent: :destroy
   has_many_attached :images, dependent: :destroy
   validates :title, uniqueness: true, presence: true

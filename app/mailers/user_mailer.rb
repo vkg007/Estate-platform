d3e7d414832175598d6049dc24f721e669c1bf36
vkg007 <vkg0072013@gmail.com>
2021-03-24 13:31:32 +0530

    Email Acknowledgment integrated

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+  default from: 'vivekrails007@gmail.com'
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def contract_email(contract)
+    @contract = contract
+    mail(to: @contract.user.email, subject: 'Contract Agreement')
+  end
+end

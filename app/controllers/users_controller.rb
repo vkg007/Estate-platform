8ecd4f692b6833cd71140e8ffe68f05269208981
Vivek Gupta <55463168+vkg007@users.noreply.github.com>
2021-03-24 16:15:52 +0530

    Email Acknowledgment integrated (#10)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome_email(@user).deliver_now
       session[:user_id] = @user.id
       flash[:success] = 'You have successfully created an account!'
       redirect_to root_path

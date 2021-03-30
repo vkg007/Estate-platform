e6dc2ceac11ed2fe7ef4f02d962599f3152c348f
vkg007 <vkg0072013@gmail.com>
2021-03-30 16:16:13 +0530

    Rspec testing

---
+++ spec/models/contract_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Contract, type: :model do
+  describe 'validation tests' do
+    it { should validate_presence_of(:final_price) }
+    it { should validate_presence_of(:start_date) }
+    it { should validate_presence_of(:end_date) }
+    it { should validate_presence_of(:transaction_id) }
+    it { should validate_uniqueness_of(:transaction_id) }
+    it { should validate_presence_of(:payment_mode) }
+    it { should validate_presence_of(:contract_type) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:property) }
+  end
+
+  describe 'scopes' do
+    it 'return contract object with id with property field and user field' do
+      
+    end
+    it { should belong_to(:property) }
+  end
+end
